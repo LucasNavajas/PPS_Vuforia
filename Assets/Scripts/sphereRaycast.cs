3fb250fa2b3146aa32bb9c37a8b9695c8573501e
Lucas Navajas <61892711+LucasNavajas@users.noreply.github.com>
2023-09-11 12:34:30 -0300

    Avances estilos botones, zoom esfera

--- Assets/Scripts/sphereRaycast.cs
+++ Assets/Scripts/sphereRaycast.cs
@@ -1,32 +1,99 @@
 using UnityEngine;
+using Vuforia;
 
 public class SphereRaycast : MonoBehaviour
+
 {
+
+    private Camera arCamera;
+    private Transform targetTransform;
+    private bool isZoomed = false;
+    public Canvas canvas1; // Asigna el Canvas 1 en el Inspector
+
+    private void Start()
+    {
+        arCamera = Camera.main;
+        targetTransform = transform.parent; // Obtén el Transform del contenedor del modelo 3D
+        canvas1.enabled = false;
+
+    }
+
+    private void OnMouseDown()
+    {
+        if (!isZoomed)
+        {
+            // Obtén la posición de la esfera en el espacio del mundo
+            Vector3 spherePosition = transform.position;
+
+            // Calcula una nueva posición para la cámara que enfoque la esfera
+            Vector3 cameraPosition = spherePosition - arCamera.transform.forward * 2f; // Puedes ajustar la distancia de zoom según tus preferencias
+
+            // Mueve la cámara a la nueva posición
+            arCamera.transform.position = cameraPosition;
+
+            // Ajusta la posición del modelo 3D para que coincida con la esfera
+            targetTransform.position = spherePosition;
+
+            canvas1.enabled = true; // Activa el Canvas 1
+
+
+            isZoomed = true;
+        }
+        else
+        {
+            Camera.main.transform.position = Vector3.zero; // O la posición inicial de tu cámara AR
+                                                           // Devuelve la posición y escala del modelo 3D a su estado original
+            targetTransform.localPosition = Vector3.zero; // O la posición inicial del modelo 3D
+            targetTransform.localScale = Vector3.one; // O la escala inicial del modelo 3D
+            canvas1.enabled = false; // Desactiva el Canvas 1
+            isZoomed = false;
+        }
+    }
     void Update()
     {
-        // Comprobar si hay al menos un toque en la pantalla.
+        // Verificar si hay toques en la pantalla
         if (Input.touchCount > 0)
         {
-            // Obtener el primer toque (índice 0).
+            // Obtener el primer toque (puedes iterar sobre Input.touches para manejar múltiples toques)
             Touch touch = Input.GetTouch(0);
 
-            // Comprobar si el toque ha comenzado.
+            // Verificar si el toque está en fase "Began" (comenzó)
             if (touch.phase == TouchPhase.Began)
             {
-                // Aquí puedes realizar acciones cuando el toque comienza.
-
-                // Realizar raycasting en función de las coordenadas del toque.
+                // Realizar acciones cuando se toca la pantalla
+                // Por ejemplo, puedes verificar si el toque colisiona con el objeto
                 Ray ray = Camera.main.ScreenPointToRay(touch.position);
                 RaycastHit hit;
 
                 if (Physics.Raycast(ray, out hit))
                 {
-                    // Comprobar si el rayo colisiona con la esfera u otro objeto deseado.
-                    if (hit.collider.CompareTag("SphereTag"))
+                    if (hit.collider.gameObject == gameObject)
                     {
-                        // El toque ha comenzado en la esfera.
-                        // Puedes realizar acciones específicas aquí.
-                        print("Toque comenzado en la esfera");
+                        if (!isZoomed)
+                        {
+                            // Obtén la posición de la esfera en el espacio del mundo
+                            Vector3 spherePosition = transform.position;
+
+                            // Calcula una nueva posición para la cámara que enfoque la esfera
+                            Vector3 cameraPosition = spherePosition - arCamera.transform.forward * 2f; // Puedes ajustar la distancia de zoom según tus preferencias
+
+                            // Mueve la cámara a la nueva posición
+                            arCamera.transform.position = cameraPosition;
+
+                            // Ajusta la posición del modelo 3D para que coincida con la esfera
+                            targetTransform.position = spherePosition;
+                            canvas1.enabled = true; // Activa el Canvas 1
+                            isZoomed = true;
+                        }
+                        else
+                        {
+                            Camera.main.transform.position = Vector3.zero; // O la posición inicial de tu cámara AR
+                            // Devuelve la posición y escala del modelo 3D a su estado original
+                            targetTransform.localPosition = Vector3.zero; // O la posición inicial del modelo 3D
+                            targetTransform.localScale = Vector3.one; // O la escala inicial del modelo 3D
+                            canvas1.enabled = false; // Desactiva el Canvas 1
+                            isZoomed = false;
+                        }
                     }
                 }
             }
